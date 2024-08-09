d7ef3883c771264b633f6ef05cd6d8cd0c461aa6
Zayyan Naveed <zayyan.naveed@yale.edu>
2024-08-09 13:18:52 -0400

    added openrouterapi to use llmama instead of gpt4o

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -13,10 +13,15 @@ Escalate complex issues to human support when necessary, ensuring a smooth hando
 Continuously update users about any ongoing maintenance or updates on the platform.
 Your goal is to enhance the user experience by providing prompt, accurate, and helpful responses, ensuring that users feel supported and confident in using Headstarter AI for their software engineering job interviews.`;
 
-
 export async function POST(req) {
-    const openai = new OpenAI();
+
     const data = await req.json();
+    const openai = new OpenAI(
+        {
+        baseURL: "https://openrouter.ai/api/v1",
+        apiKey: process.env.OPENROUTER_API_KEY,
+    }
+    );
 
     const completion = await openai.chat.completions.create({
         messages : [{
@@ -25,13 +30,15 @@ export async function POST(req) {
             }, 
             ...data,
         ],
-        model: 'gpt-4o-mini',
+        // model: "meta-llama/llama-3.1-8b-instruct:free",
+        model: "qwen/qwen-2-7b-instruct:free",
         stream: true,
     })
 
     const stream = new ReadableStream({
         async start(controller) {
             const encoder = new TextEncoder();
+            
             try {
                 for await(const chunk of completion) {
                     const content = chunk.choices[0]?.delta?.content;
@@ -47,7 +54,43 @@ export async function POST(req) {
             }
         }
     })
-
-    return new NextResponse(stream)
-
+    return new NextResponse(stream);
 }
+
+
+// export async function POST(req) {
+
+//     const data = await req.json();
+
+//     const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+//         method: "POST",
+//         headers: {
+//             "Authorization": `Bearer ${process.env.OPENROUTER_API_KEY}`,
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify({
+//             "model": 'meta-llama/llama-3.1-8b-instruct:free',
+//             "messages": [
+//                 {
+//                     "role": 'system',
+//                     "content": systemPrompt
+//                 },
+//                 ...data,
+//             ],
+//         })
+//     });
+
+//     if (!response.ok) {
+//         const errorDetails = await response.text();
+//         console.error('Error Details:', errorDetails);
+//         throw new Error(`Failed to fetch from OpenRouter API: ${response.status} ${response.statusText}`);
+//     }
+//     const responseData = await response.json();
+//     const content = responseData.choices[0].message.content;
+//     console.log("Extracted Content:", content);
+
+//     return new NextResponse(content, {
+//         headers: { 'Content-Type': 'text/plain' }
+//     });
+
+// }
